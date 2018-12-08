997b2513dbde271ca6040a51a9a8360dca309538
jasonawj <675848599@qq.com>
2018-12-08 12:51:39 +0800

    修改了user1111

--- git_day01/src/main/java/com/itheima/domain/User.java
+++ git_day01/src/main/java/com/itheima/domain/User.java
@@ -5,4 +5,36 @@ public class User {
     private String sex;
     private Integer age;
     private String size;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
